e1fb9964249ee612e5c383d0a2b3c8310c77faff
Vinoth07 <sangaralingamvinoth64@gmail.com>
2024-10-30 12:03:30 +0530

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+// Route::get('/users', Index::class)->name('users');
+// Route::get('/users/create', UserCreate::class)->name('users.create');
+// Route::get('/users/{user}/edit', UserUpdate::class)->name('users.edit');
